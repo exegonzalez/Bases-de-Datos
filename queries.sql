f00a5c05994a9655837b4c75ddd51290f73f7716
Exequiel <exe.gonzalez14@hotmail.com>
2019-11-21 01:04:04 -0300

    consulta 4,5 y script vistas

--- queries.sql
+++ queries.sql
@@ -4,10 +4,11 @@
 select u.email, u.nombre, u.apellido, u.nombreuser, f.cantidad_compras from UsuarioMayorCantidadCompras('2000-01-01','2020-01-01') f, usuario u where u.email=f.usuario;
 
 -- 2. Producto del cual se vendieron más unidades -- 
-CREATE VIEW lineaProductos AS SELECT * FROM linea WHERE producto is not null;
+select p.codigo, p.nombre, p.tipo, p.proveedor, s.unidades_compradas from sumaProductos s, producto p where s.producto=p.codigo;
 
-CREATE VIEW sumaProductos AS SELECT lp1.producto, SUM(lp1.cantidadproducto) AS unidades_compradas
-FROM lineaProductos lp1 GROUP BY lp1.producto ORDER BY unidades_compradas DESC LIMIT 1;
+-- 4. Proveedor que provee más productos
+select p.cuit, p.nombre, p.email, p.direccion, pmp.productos_provistos from proveedor p, proveedorMasProductos pmp where pmp.proveedor=p.cuit;
 
-select p.codigo, p.nombre, p.tipo, p.proveedor, s.unidades_compradas from sumaProductos s, producto p where s.producto=p.codigo
+-- 5. Listado de productos que pertenecen a más de un combo
+select p.codigo, p.nombre, p.tipo, p.proveedor from productosMuchosCombos pmc, producto p where pmc.producto=p.codigo;
 */
