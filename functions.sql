18795361d3219375f9e46e873cf2fe097b4a812a
Exequiel <exe.gonzalez14@hotmail.com>
2019-11-20 22:40:57 -0300

    segunda consulta terminada

--- functions.sql
+++ functions.sql
@@ -1,2 +1,13 @@
+/*
 -- Funciones --
 -- 1. Usuario que realizo más compras ente dos fechas --
+CREATE OR REPLACE FUNCTION UsuarioMayorCantidadCompras(date, date) RETURNS TABLE (usuario varchar(255), cantidad_compras bigint) AS
+$BODY$
+DECLARE
+BEGIN
+	return query (SELECT c.usuario, count(c.usuario) AS mayor_comprador FROM compra c WHERE c.fecha BETWEEN $1 and $2
+			 GROUP BY c.usuario ORDER BY mayor_comprador DESC LIMIT 1);
+end
+$BODY$
+LANGUAGE 'plpgsql';
+*/
--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
+/*
 -- Consultas --
 -- 1. Usuario que realizo más compras ente dos fechas --
+select u.email, u.nombre, u.apellido, u.nombreuser, f.cantidad_compras from UsuarioMayorCantidadCompras('2000-01-01','2020-01-01') f, usuario u where u.email=f.usuario;
 
+-- 2. Producto del cual se vendieron más unidades -- 
+CREATE VIEW lineaProductos AS SELECT * FROM linea WHERE producto is not null;
+
+CREATE VIEW sumaProductos AS SELECT lp1.producto, SUM(lp1.cantidadproducto) AS unidades_compradas
+FROM lineaProductos lp1 GROUP BY lp1.producto ORDER BY unidades_compradas DESC LIMIT 1;
+
+select p.codigo, p.nombre, p.tipo, p.proveedor, s.unidades_compradas from sumaProductos s, producto p where s.producto=p.codigo
+*/
