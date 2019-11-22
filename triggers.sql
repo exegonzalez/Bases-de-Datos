41348f446e3b15a3e0e9571ee4358af427e7f8e4
Exequiel <exe.gonzalez14@hotmail.com>
2019-11-22 18:29:20 -0300

    Trigger de que se haya comprado el producto para poder calificar listo

--- triggers.sql
+++ triggers.sql
@@ -3,8 +3,6 @@
 -- 1. Controlar que cuando se carga una línea solo puede haber un producto o combo, no ambos --
 CREATE OR REPLACE FUNCTION controlLinea() RETURNS TRIGGER AS $funcemp$
 DECLARE
-	combo integer ;
-	producto integer;
 BEGIN
 	IF ((NEW.producto is null) and (NEW.combo is not null)) THEN
 		RETURN NEW;
@@ -18,6 +16,24 @@ END; $funcemp$ LANGUAGE plpgsql;
 CREATE TRIGGER triggerControlLinea BEFORE INSERT OR UPDATE ON linea
 FOR EACH ROW EXECUTE PROCEDURE controlLinea();
 
+
+-- 2. Controlar que para calificar un producto, se debe haber realizado una compra del mismo previamente --
+CREATE OR REPLACE FUNCTION controlCalificacionCompra() RETURNS TRIGGER AS $funcemp$
+DECLARE
+productoCompra bigint;
+BEGIN
+	productoCompra:= (select count(lp.producto) from compra co, carrito ca, lineaProductos lp
+	WHERE NEW.usuario=co.usuario and ca.codigo=co.carrito and ca.codigo=lp.carrito and lp.producto=NEW.producto);
+	IF (productoCompra!=0) THEN
+		RETURN NEW;
+	ELSE
+		RAISE EXCEPTION 'Debe realizar alguna compra del producto para poder calificarlo';
+	END IF;
+END; $funcemp$ LANGUAGE plpgsql;
+
+CREATE TRIGGER triggerControlCalificacionCompra BEFORE INSERT OR UPDATE ON calificacion
+FOR EACH ROW EXECUTE PROCEDURE controlCalificacionCompra();
+	
 -- Actualiza la calificacion del producto cuando se inserta un comentario
 create or replace function actualizarCalificacion() returns trigger as $$
 declare
